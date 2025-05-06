761f279909bf4c4c1d19d9a2511e09616dbc915b
dalilaxav <dalilaxavier05@gmail.com>
2025-05-06 15:50:20 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const botao = document.getElementById("botao");
+const divMensagem = document.getElementById("mensagem");
+
+botao.addEventListener("click", function (){
+    divMensagem.textContent = "Seja bem-vindo(a)! Nesta página compartilho um pouco sobre mim, meus hobbies e lugares que sonho conhecer. Aproveite a visita!"
+});
